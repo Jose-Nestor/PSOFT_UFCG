d6c27739ed0f8aa98bf5afe4d63e348269b07637
nestor <jose.nestor.combe@ccc.ufcg.edu.br>
2023-03-27 09:39:59 -0300

    anotacao aula

---
+++ mercadofacil/src/test/java/com/ufcg/psoft/mercadofacil/service/ProdutoAlterarServiceTest.java
@@ -0,0 +1,51 @@
+package com.ufcg.psoft.mercadofacil.service;
+
+import com.ufcg.psoft.mercadofacil.model.Produto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProdutoAlterarServiceTest {
+
+
+    ProdutoRepository<Produto, Long> produtoRepository;
+
+    Produto produto;
+
+    @BeforeEach
+    void setup() {
+        Mockito.when(produtoRepository.find(10L))
+                .thenReturn(Produto.builder()
+                        .id(10L)
+                        .codigoBarra("7899854126547")
+                        .nome("Produto Dez")
+                        .fabricante("Empresa Dez")
+                        .preco(450.00)
+                        .build()
+                );
+
+        produto = produtoRepository.find(10L);
+        Mockito.when(produtoRepository.update(produto))
+                .thenReturn(Produto.builder()
+                        .id(10L)
+                        .codigoBarra("7899854126547")
+                        .nome("Nome Produto Alterado")
+                        .fabricante("Nome Fabricante Alterado")
+                        .preco(500.00)
+                        .build()
+                );
+    }
+
+    @Test
+    @DisplayName("Quando altero o nome do produto com um nome válido")
+    void alterarNomeDoProduto(){
+
+        produto.setNome("Nome Produto Alterado");
+
+        Produto resultado = driver.alterar(produto);
+        assertEquals("Nome Produto Alterado", resultado.getNome());
+    }
+}
