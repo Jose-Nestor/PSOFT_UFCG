92c5c589f01aa89182b34ef968dffdc265338588
nestor <jose.nestor.combe@ccc.ufcg.edu.br>
2023-03-29 11:50:23 -0300

    novos testes

--- mercadofacil/src/main/java/com/ufcg/psoft/mercadofacil/service/ProdutoAlterarImplService.java
+++ mercadofacil/src/main/java/com/ufcg/psoft/mercadofacil/service/ProdutoAlterarImplService.java
@@ -4,12 +4,16 @@ import com.ufcg.psoft.mercadofacil.model.Produto;
 import com.ufcg.psoft.mercadofacil.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class ProdutoAlterarImplService implements ProdutoAlterarService{
+public class ProdutoAlterarImplService
+        implements ProdutoAlterarService{
     @Autowired
     ProdutoRepository<Produto, Long> produtoRepository;
 
     @Override
     public Produto alterar(Produto produtoAlterado){
+        if (produtoAlterado.getPreco()<=0){
+            throw new RuntimeException("Preco invalido!");
+        }
         return produtoRepository.update(produtoAlterado);
     }
 }
